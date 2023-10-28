b618e33a18e81b734232b0d4ddf07d7cc97e6fa3
Khalid Magdy <km373413@gmai,com>
2023-10-28 00:20:46 -0700

    commition

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     k = 0
-    for k in range(x):
+    for i in range(x):
         try:
-            print(my_list[k], end="")
+            print(my_list[i], end="")
             k += 1
         except IndexError:
             break
     print("")
-    Return
+    return
 

01028d8c0ed8b527df1295f883dc08f6c9662e88
Khalid Magdy <km373413@gmai,com>
2023-10-29 14:01:23 -0700

    tasks commit 123

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""the Square module."""
+
+
+class Square:
+    """Defines a square & attrs."""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of the square side.
+        """
+        self.__size = size
+
